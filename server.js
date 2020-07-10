3d715fe29f7e70171aa3d0e8f947c95a21233af5
Tom Black <tom.black06007@gmail.com>
2020-07-09 17:01:36 -0700

    Made some tweaks to the mongoose connection for heroku.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
+
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
